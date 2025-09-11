2f78c610aea9c3c07ab6c83da34d19da9d8f9083
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-09-11 13:28:51 +0200

    Update test_intellisense.py

--- test_intellisense.py
+++ test_intellisense.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def test_values():
     with testbook('2_IntelliSense.ipynb', execute=True) as tb:
-        array1 = tb.value('list(array1)')
-        assert array1[0]==4.0, f"array1[0]={array1[0]} of task 3.1 is not equal to 4"
+        array1 = tb.value('array1[0]')
+        assert array1==4.0, f"array1[0]={array1} of task 3.1 is not equal to 4"
 
         newtext = tb.ref('newtext')
         assert newtext == "hello MUDE", f"newtext={newtext} of task 3.2 is not equal to 'hello MUDE'"
